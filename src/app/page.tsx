7c16dfd832b79a439686d75b3d25948fce6c9202
jairo cabarcas <jairocabarcas@gmail.com>
2025-09-23 10:00:14 -0500

    cambios en el fondo de la tarjeta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import RSVPForm from "@/app/components/RSVPForm";
 export default function Home() {
   return (
       <div className="min-h-screen bg-[#fdfbf7] flex items-center justify-center px-4 py-10">
-        <div className="max-w-xl w-full bg-white shadow-2xl p-6 md:p-10">
+        <div className="max-w-xl w-full bg-fondo bg-repeat bg-cover shadow-2xl p-6 md:p-10">
           <div className="w-full border-5 border-[#E0D5C3] p-5">
               <HeaderLogo />
               <InvitationText />

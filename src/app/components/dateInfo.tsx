724052b1a011032d0d115d1bd023a9fc32a93d34
jairo cabarcas <jairocabarcas@gmail.com>
2025-09-22 23:07:51 -0500

    Fix: correciones en los estilos

--- src/app/components/dateInfo.tsx
+++ src/app/components/dateInfo.tsx
@@ -1,8 +1,8 @@
 import {Playfair_Display} from "next/font/google";
 
 const playFairDisplay = Playfair_Display({
-    weight: ['400', '400', '700', '700'], // 400 = normal, 700 = negrita
-    style: ['normal', 'italic', 'normal', 'italic'], // normal e italic
+    weight: ['400', '400','500', '500', '700', '700'], // 400 = normal, 700 = negrita
+    style: ['normal', 'italic', 'normal', 'italic', 'normal', 'italic'], // normal e italic
     subsets: ['latin'],
 
 })
@@ -11,18 +11,18 @@ export default function DateInfo() {
     return (
         <div className="text-center my-5">
             <div className="flex items-center justify-center w-full gap-5 h-fit">
-                <div className="h-full border-y-2 border-y-[#937552] flex items-end justify-center">
-                    <span className="uppercase text-xs md:text-sm tracking-widest text-[#937552]">
+                <div className="h-full  flex items-center justify-center md:pt-7">
+                    <span className="uppercase px-2 border-y-2 border-y-[#937552] text-xs md:text-lg tracking-widest text-[#937552]">
                         Sábado
                     </span>
                 </div>
                 <div className="h-full flex items-center justify-center">
-                    <span className={`${playFairDisplay.className} text-4xl md:text-7xl font-bold text-[#937552]`}>
+                    <span className={`${playFairDisplay.className} text-4xl md:text-8xl font-medium  text-[#937552] text-center`}>
                       10
                     </span>
                 </div>
-                <div className="h-full border-y-2 border-y-[#937552]">
-                    <span className="uppercase text-xs md:text-sm tracking-widest text-[#937552]">
+                <div className="h-full flex items-center justify-center md:pt-7">
+                    <span className="uppercase px-2 border-y-2 border-y-[#937552] text-xs md:text-lg tracking-widest text-[#937552]">
                       Enero
                     </span>
                 </div>

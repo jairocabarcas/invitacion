541df70f52f0f6d57a1be8db4af4dda7fa7510cf
jairo cabarcas <jairocabarcas@gmail.com>
2025-10-02 13:44:19 -0500

    modificacion de los icono de localizacion

--- src/app/components/ButtonLocation.tsx
+++ src/app/components/ButtonLocation.tsx
@@ -9,17 +9,17 @@ export default function ButtonLocation() {
                 rel="noopener noreferrer"
                 className="flex items-center gap-1 justify-center md:space-x-2 bg-[#937552] text-white md:px-5 md:py-2 px-2 py-1 rounded-full shadow hover:bg-[#9c8566] transition text-sm md:text-base"
             >
-                {/*<MapPin className="h-3 w-4 md:h-auto"/>*/}
+                <MapPin className="h-3 w-4 md:h-auto"/>
                 <span>Ceremonia</span>
             </a>
-            <MapPin className=" md:h-auto text-[#937552]"/>
+            {/*<MapPin className=" md:h-auto text-[#937552]"/>*/}
             <a
                 href="https://maps.app.goo.gl/CpNNfJp1A5asJit87"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex items-center gap-1 justify-center md:space-x-2 bg-[#937552] text-white md:px-5 md:py-2 px-2 py-1 rounded-full shadow hover:bg-[#9c8566] transition text-sm md:text-base"
             >
-                {/*<MapPin className="h-3 w-4 md:h-auto"/>*/}
+                <MapPin className="h-3 w-4 md:h-auto"/>
                 <span>Recepción</span>
             </a>
         </div>
